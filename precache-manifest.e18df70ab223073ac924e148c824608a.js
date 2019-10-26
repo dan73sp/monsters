b1751734445b02e8f4099db3f4e8ae965da47426
Daniel De Almeida <dan73sp@gmail.com>
2019-10-26 14:01:51 +0200

    Updates

---
+++ precache-manifest.e18df70ab223073ac924e148c824608a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124446a0aae3ca73a24f7453cbc9f834",
+    "url": "/dan73sp/monsters/index.html"
+  },
+  {
+    "revision": "544126595e8e2eb52a71",
+    "url": "/dan73sp/monsters/static/css/main.e6f2ea49.chunk.css"
+  },
+  {
+    "revision": "849a0e77c7a65ebc01db",
+    "url": "/dan73sp/monsters/static/js/2.64cdb3b7.chunk.js"
+  },
+  {
+    "revision": "544126595e8e2eb52a71",
+    "url": "/dan73sp/monsters/static/js/main.b69fd73a.chunk.js"
+  },
+  {
+    "revision": "daae7695216b22712b1d",
+    "url": "/dan73sp/monsters/static/js/runtime-main.0d9dd999.js"
+  }
+]);
